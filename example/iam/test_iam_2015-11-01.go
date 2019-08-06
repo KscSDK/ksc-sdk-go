1d75790da46011e6dab0d3fcddd935d4594b7400
yangzechao <yangzechao@kingsoft.com>
2019-08-06 17:08:18 +0800

    optimize code

--- example/iam/test_iam_2015-11-01.go
+++ example/iam/test_iam_2015-11-01.go
@@ -9,17 +9,19 @@ import (
 )
 
 func main() {
+	ak := "IDAKLT0tdvdrASSnGbxQbHSdeAbA"
+	sk := "ODi8E02pdZSO1pGyAWh8+OHF6Uh9LIxv9xhYVtdgsN9IHetRJJvPWttQPxvIpJi+7Q=="
+	region := "cn-beijing-6"
+
 	/*
-	ak := "你的ak"
-	sk := "你的sk"
-	*/
-	ak := "AKLT2mjX47yFSp2leXlY8h0dVA"
-	sk := "ODFsox160Mo4Qz3PX5zyYmIbmxFx4qhfbRBxtBH7urMolwxLxUdUoN+oPt5lRaMt8w=="
+	ak := "IDAKLTWjcmVntESASmyr9QAGELMg"
+	sk := "OKn4wzC1ncrDq5tYBFZWeESIuh5i1twyZm5+/lfNRdMr6ZlPwHNy0Eu3YUL+wACSIA=="
 	region := "cn-shanghai-3"
+	*/
 
 	//debug模式的话 打开这个开关
-	svc := iam.SdkNew(ksc.NewClient(ak, sk /*,true*/), &ksc.Config{Region: &region}, &utils.UrlInfo{
-		UseSSL: false,
+	svc := iam.SdkNew(ksc.NewClient(ak, sk ,true), &ksc.Config{Region: &region}, &utils.UrlInfo{
+		UseSSL: true,
 	})
 
 	var resp *map[string]interface{}
@@ -39,7 +41,7 @@ func main() {
 
 	//	****************************************查询用户基本信息（GetUser）***************************************************
 	params := make(map[string]interface{})
-	params["UserName"] = "zechao1"
+	params["UserName"] = "one"
 
 	resp, err = svc.GetUser(&params)
 	if err != nil {
