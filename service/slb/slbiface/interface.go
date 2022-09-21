fd75c8b21c9462afe798b0681333c82e0d9b5a6d
x6doooo <x6doooo@gmail.com>
2022-09-21 09:18:16 +0800

    update: 增加slb接口

--- service/slb/slbiface/interface.go
+++ service/slb/slbiface/interface.go
@@ -23,37 +23,37 @@ import (
 // can be stubbed out for unit testing your code with the SDK without needing
 // to inject custom request handlers into the SDK's request pipeline.
 //
-//    // myFunc uses an SDK service client to make a request to
-//    // slb.
-//    func myFunc(svc slbiface.SlbAPI) bool {
-//        // Make svc.AssociateLoadBalancerAcl request
-//    }
+//	// myFunc uses an SDK service client to make a request to
+//	// slb.
+//	func myFunc(svc slbiface.SlbAPI) bool {
+//	    // Make svc.AssociateLoadBalancerAcl request
+//	}
 //
-//    func main() {
-//        sess := session.New()
-//        svc := slb.New(sess)
+//	func main() {
+//	    sess := session.New()
+//	    svc := slb.New(sess)
 //
-//        myFunc(svc)
-//    }
+//	    myFunc(svc)
+//	}
 //
 // In your _test.go file:
 //
-//    // Define a mock struct to be used in your unit tests of myFunc.
-//    type mockSlbClient struct {
-//        slbiface.SlbAPI
-//    }
-//    func (m *mockSlbClient) AssociateLoadBalancerAcl(input *map[string]interface{}) (*map[string]interface{}, error) {
-//        // mock response/functionality
-//    }
+//	// Define a mock struct to be used in your unit tests of myFunc.
+//	type mockSlbClient struct {
+//	    slbiface.SlbAPI
+//	}
+//	func (m *mockSlbClient) AssociateLoadBalancerAcl(input *map[string]interface{}) (*map[string]interface{}, error) {
+//	    // mock response/functionality
+//	}
 //
-//    func TestMyFunc(t *testing.T) {
-//        // Setup Test
-//        mockSvc := &mockSlbClient{}
+//	func TestMyFunc(t *testing.T) {
+//	    // Setup Test
+//	    mockSvc := &mockSlbClient{}
 //
-//        myfunc(mockSvc)
+//	    myfunc(mockSvc)
 //
-//        // Verify myFunc's functionality
-//    }
+//	    // Verify myFunc's functionality
+//	}
 //
 // It is important to note that this interface will have breaking changes
 // when the service model is updated and adds new API operations, paginators,
@@ -164,6 +164,10 @@ type SlbAPI interface {
 	DescribeLoadBalancerAclsWithContext(aws.Context, *map[string]interface{}, ...request.Option) (*map[string]interface{}, error)
 	DescribeLoadBalancerAclsRequest(*map[string]interface{}) (*request.Request, *map[string]interface{})
 
+	DescribeLoadBalancerAttributes(*map[string]interface{}) (*map[string]interface{}, error)
+	DescribeLoadBalancerAttributesWithContext(aws.Context, *map[string]interface{}, ...request.Option) (*map[string]interface{}, error)
+	DescribeLoadBalancerAttributesRequest(*map[string]interface{}) (*request.Request, *map[string]interface{})
+
 	DescribeLoadBalancers(*map[string]interface{}) (*map[string]interface{}, error)
 	DescribeLoadBalancersWithContext(aws.Context, *map[string]interface{}, ...request.Option) (*map[string]interface{}, error)
 	DescribeLoadBalancersRequest(*map[string]interface{}) (*request.Request, *map[string]interface{})
@@ -215,6 +219,10 @@ type SlbAPI interface {
 	ModifyLoadBalancerAclEntry(*map[string]interface{}) (*map[string]interface{}, error)
 	ModifyLoadBalancerAclEntryWithContext(aws.Context, *map[string]interface{}, ...request.Option) (*map[string]interface{}, error)
 	ModifyLoadBalancerAclEntryRequest(*map[string]interface{}) (*request.Request, *map[string]interface{})
+
+	ModifyLoadBalancerAttributes(*map[string]interface{}) (*map[string]interface{}, error)
+	ModifyLoadBalancerAttributesWithContext(aws.Context, *map[string]interface{}, ...request.Option) (*map[string]interface{}, error)
+	ModifyLoadBalancerAttributesRequest(*map[string]interface{}) (*request.Request, *map[string]interface{})
 
 	ModifySlbRule(*map[string]interface{}) (*map[string]interface{}, error)
 	ModifySlbRuleWithContext(aws.Context, *map[string]interface{}, ...request.Option) (*map[string]interface{}, error)
